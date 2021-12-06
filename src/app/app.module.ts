16d6508f27c58f3bc2b3d8ca23bd09aed83ad4b0
Leonardo Cruz <leonardo.m.cruz@hotmail.com>
2021-12-06 18:09:36 -0300

    Adicionado componentes de login e de registro ao modulo app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UnregisteredUserComponent } from './unregistered-user/unregistered-user.component';
+import { LoginComponent } from './unregistered-user/login/login.component';
+import { RegisterComponent } from './unregistered-user/register/register.component';
 
 @NgModule({
   declarations: [	
     AppComponent,
-      UnregisteredUserComponent
+      UnregisteredUserComponent,
+      LoginComponent,
+      RegisterComponent
    ],
   imports: [
     BrowserModule,
