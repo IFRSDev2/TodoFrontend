5c6c828725eebc6d5c2dcbe401e60e84a1caaf3c
Leonardo Cruz <leonardo.m.cruz@hotmail.com>
2021-12-06 15:22:16 -0300

    Adicionado componentes as rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
+import { UnregisteredUserComponent } from './unregistered-user/unregistered-user.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './unregistered-user/login/login.component';
+import { RegisterComponent } from './unregistered-user/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+  {
+    path: 'home',
+    component: UnregisteredUserComponent,
+    children: [
+      {
+        path: 'login',
+        component: LoginComponent
+      },
+      {
+        path: 'register',
+        component: RegisterComponent
+      },
+      {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UnregisteredUserComponent } from './unregistered-user/unregistered-user.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+      UnregisteredUserComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
