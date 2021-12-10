54ae544a9117503af42b7ef65e1fd660c5eaaea2
Leonardo Cruz <leonardo.m.cruz@hotmail.com>
2021-12-10 20:24:22 -0300

    Adicionado formulario de login no componente e requisicao iniciais

--- src/app/unregistered-user/register/register.component.ts
+++ src/app/unregistered-user/register/register.component.ts
@@ -1,4 +1,7 @@
+import { AuthService } from './../services/auth.service';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  formularioDeCadastro: FormGroup = new FormGroup({
+    nome: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    emailConfirmacao: new FormControl('', [Validators.required, Validators.email]),
+    senha: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    senhaConfirmacao: new FormControl('', [Validators.required, Validators.minLength(6)])
+  });
+
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
+
+  }
+
+  onSubmit(userData: any) {
+    console.log(userData);
+    
+    if (!this.verifyIfPasswordsMatch()) {
+      this.formularioDeCadastro.setErrors({senhasDiferentes: true});
+      return;
+    }
+
+    this.authService.cadastro(userData.nome, userData.email, userData.emailConfirmacao, userData.senha, userData.senhaConfirmacao).subscribe(
+      (response) => {
+        console.log(response);
+        this.router.navigate(['/login']);
+      },
+      (error) => {
+        console.log(error);
+
+      }
+    );
+  }
+
+  verifyIfPasswordsMatch() {
+    const senha = this.formularioDeCadastro.get('senha');
+    const senhaConfirmacao = this.formularioDeCadastro.get('senhaConfirmacao');
+
+    if (senha?.value === senhaConfirmacao?.value) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
 }
