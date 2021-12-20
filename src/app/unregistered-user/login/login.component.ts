1f643c766fc50ec7718d090889b449f323f4ef0f
diogobertotto <diogobertotto@hotmail.com>
2021-12-20 16:22:53 -0300

    definindo campos do componente do login

--- src/app/unregistered-user/login/login.component.ts
+++ src/app/unregistered-user/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { EmailValidator, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from './../services/auth.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  formularioDeLogin: FormGroup = new FormGroup({    
+    email: new FormControl('', [Validators.required, Validators.email]),
+    senha: new FormControl('', [Validators.required, Validators.minLength(6)]),
+  });
+
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
+
   }
 
+  onSubmit(userData: any) {
+    console.log(userData);    
+
+    this.authService.login(userData.email, userData.senha).subscribe(
+      (response) => {
+        console.log(response);
+        this.router.navigate(['/login']);
+      },
+      (error) => {
+        console.log(error);
+
+      }
+    );
+  }
 }
