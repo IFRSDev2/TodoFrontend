91af46e7f73c8c9321157792a526d819e04a6502
diogobertotto <diogobertotto@hotmail.com>
2022-02-07 11:47:11 -0300

    ajuste no formulario de cadastro de tarefas

--- src/app/registered-user/pages/home/components/taskForm/taskForm.component.ts
+++ src/app/registered-user/pages/home/components/taskForm/taskForm.component.ts
@@ -1,18 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Tarefa } from 'src/app/registered-user/interfaces/tarefa';
 import { TaskService } from 'src/app/registered-user/services/task.service';
+
 
 @Component({
   selector: 'app-taskForm',
   templateUrl: './taskForm.component.html',
   styleUrls: ['./taskForm.component.scss']
 })
+
 export class TaskFormComponent implements OnInit {
+  
   formularioDaTarefa: FormGroup = new FormGroup({
-    nomeTarefa: new FormControl('', []),
-    tipoTarefa: new FormControl('', []),
-    local: new FormControl('', []),
+    nomeTarefa: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    tipoTarefa: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    localizacaoTarefa: new FormControl('', [Validators.required]),
 
   });
 
